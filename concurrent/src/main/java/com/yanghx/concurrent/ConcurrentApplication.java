c6df5542a3fb1ccdb0399612d177c094b92d1907
yanghx <yanghx_mail@163.com>
2019-05-03 17:18:36 +0800

    初始化

---
+++ concurrent/src/main/java/com/yanghx/concurrent/ConcurrentApplication.java
@@ -0,0 +1,16 @@
+package com.yanghx.concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangheng
+ */
+@SpringBootApplication
+public class ConcurrentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentApplication.class, args);
+    }
+
+}
